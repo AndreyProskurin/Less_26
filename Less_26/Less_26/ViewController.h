8de64464fb0fc03b4657e7bffc51eee45e123e22
Andrey Proskurin <andrewproskurin@gmail.com>
2017-11-10 19:08:01 +0200

    single view app has been added.

---
+++ Less_26/Less_26/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Less_26
+//
+//  Created by Andrey Proskurin on 10.11.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
